5cb13bb3e057070cd62e09d1fc0b67d499929120
khanh <khanhack@outlook.com>
2020-04-15 10:51:30 +0700

    Client commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuListItemComponent } from './menu-list-item/menu-list-item.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataService } from './services/data.service';
+import { NotificationService } from './services/notification.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuListItemComponent
   ],
+  entryComponents: [],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [DataService, NotificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
